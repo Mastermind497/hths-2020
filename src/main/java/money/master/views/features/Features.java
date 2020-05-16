cfc5410f7094e5e25225201cfe6e107a8ad3a2e6
Charles <charles.jiayi.yu@gmail.com>
2020-05-16 15:38:51 -0400

    Added features.html

--- src/main/java/money/master/views/features/Features.java
+++ src/main/java/money/master/views/features/Features.java
@@ -2,14 +2,18 @@ package money.master.views.features;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import money.master.views.main.Home;
 
 @Route(value = "Features", layout = Home.class)
 public class Features extends VerticalLayout {
+
     public Features() {
         add(new H1("App Features"));
-        add(new Text("Text"));
+        add(new Text("MoneyMaster is a brand-new software that not only helps manage your finances, it sets" +
+                " one up for success in the future. Here are some of the basic features: "));
+
     }
 }
