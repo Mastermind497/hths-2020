d345c864b7feeba2a755b6f58e9b549005d2e0b4
Charles <charles.jiayi.yu@gmail.com>
2020-05-16 15:05:52 -0400

    Added private variables to spending class

--- src/main/java/money/master/backend/Spending.java
+++ src/main/java/money/master/backend/Spending.java
@@ -5,6 +5,10 @@ import java.math.BigDecimal;
 public class Spending {
     private final String     item;
     private final BigDecimal amt;
+    private boolean essential;
+    private String name;
+    private boolean change; //Gain or Lose
+    private BigDecimal localDate;
     
     public Spending(String item, BigDecimal amt) {
         this.item = item;
