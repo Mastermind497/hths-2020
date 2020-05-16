b1a332b8f9b00aecd91f03bcf81acf0395e6b648
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-16 14:38:28 -0400

    Fixed up formatting in Budget

--- src/main/java/money/master/backend/Budget.java
+++ src/main/java/money/master/backend/Budget.java
@@ -1,20 +1,19 @@
 package money.master.backend;
 
-import java.math.BigDecimal;
-
 public class Budget {
-    private Salary salary;
-    private EducationLevel edulvl;
+    private Salary         salary;
+    private EducationLevel educationLevel;
+    
     public Budget(Salary salary, EducationLevel edulvl){
-        this.salary = salary;
-        this.edulvl = edulvl;
+        this.salary         = salary;
+        this.educationLevel = edulvl;
     }
 
     public Salary getSalary() {return salary;}
 
     public void setSalary(Salary salary) {this.salary = salary;}
 
-    public EducationLevel getEdulvl() {return edulvl;}
+    public EducationLevel getEducationLevel() {return educationLevel;}
 
-    public void setEdulvl(EducationLevel edulvl) {this.edulvl = edulvl;}
+    public void setEducationLevel(EducationLevel educationLevel) { this.educationLevel = educationLevel;}
 }
