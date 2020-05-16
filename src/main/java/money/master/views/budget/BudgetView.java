17d9ffb01b25066e23a83bc52932c9c64733140c
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-16 17:10:52 -0400

    Fixed BudgetView

--- src/main/java/money/master/views/budget/BudgetView.java
+++ src/main/java/money/master/views/budget/BudgetView.java
@@ -1,5 +1,10 @@
 package money.master.views.budget;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import money.master.views.main.MainView;
 
-public class BudgetView {
+@Route(value = "profile/budget", layout = MainView.class)
+public class BudgetView extends VerticalLayout {
+
 }
