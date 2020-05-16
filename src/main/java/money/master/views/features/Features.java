bce7339e89f4647c7dd8313b5c6b193a5d000f9b
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-16 15:21:19 -0400

    Updated Featuers

--- src/main/java/money/master/views/features/Features.java
+++ src/main/java/money/master/views/features/Features.java
@@ -1,9 +1,15 @@
 package money.master.views.features;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import money.master.views.main.Home;
 
 @Route(value = "Features", layout = Home.class)
 public class Features extends VerticalLayout {
+    public Features() {
+        add(new H1("App Features"));
+        add(new Text("Text"));
+    }
 }
