e32d2728fa01edf7952c76bdf1085f0eab1c5807
Charles <charles.jiayi.yu@gmail.com>
2020-05-17 10:14:00 -0400

    Added Accordion component to LearnView

--- src/main/java/money/master/views/learn/LearnView.java
+++ src/main/java/money/master/views/learn/LearnView.java
@@ -3,7 +3,9 @@ package money.master.views.learn;
 import com.vaadin.flow.component.Text;
 
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import money.master.views.main.MainView;
@@ -11,8 +13,24 @@ import money.master.views.main.MainView;
 @Route(value = "profile-learn", layout = MainView.class)
 public class LearnView extends VerticalLayout{
     public LearnView() {
-        add(new Text("something"));
-
+        add(new Text("It's hard to manage all your finances. What if everything was in one place?"));
+        Details loans = new Details("Managing Loans",
+                new Text("Toggle using mouse, Enter and Space keys.")); // add html here
+        loans.addOpenedChangeListener(e ->
+                Notification.show(e.isOpened() ? "Opened" : "Closed"));
+        Details credit = new Details("Managing Credit",
+                new Text("Toggle using mouse, Enter and Space keys.")
+        );
+        credit.addOpenedChangeListener(e ->
+                Notification.show(e.isOpened() ? "Opened" : "Closed"));
+        Details bank = new Details("Banks",
+                new Text("Toggle using mouse, Enter and Space keys."));
+        bank.addOpenedChangeListener(e ->
+                Notification.show(e.isOpened() ? "Opened" : "Closed"));
+        Details sideCash = new Details("Making side cash",
+                new Text("Toggle using mouse, Enter and Space keys."));
+        sideCash.addOpenedChangeListener(e ->
+                Notification.show(e.isOpened() ? "Opened" : "Closed"));
 
     }
 
