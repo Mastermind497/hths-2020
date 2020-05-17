e9f206194d7af40b5e9a5613a5ddf549dc9c9682
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-17 11:10:59 -0400

    Fixed LearnView

--- src/main/java/money/master/views/learn/LearnView.java
+++ src/main/java/money/master/views/learn/LearnView.java
@@ -1,16 +1,10 @@
 package money.master.views.learn;
 
 import com.vaadin.flow.component.Text;
-
 import com.vaadin.flow.component.accordion.Accordion;
-import com.vaadin.flow.component.accordion.AccordionPanel;
-import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-
 import money.master.views.main.MainView;
 
 @Route(value = "profile-learn", layout = MainView.class)
@@ -88,8 +82,8 @@ public class LearnView extends VerticalLayout{
                         "improves your social network.\n")
         );
         accordion.add("Payment", sideCashLayout);
-
-
+        
+        add(accordion);
     }
 
 
--- src/main/java/money/master/views/main/Home.java
+++ src/main/java/money/master/views/main/Home.java
@@ -7,12 +7,13 @@ import com.vaadin.flow.router.Route;
 
 @Route(value = "", layout = HomeView.class)
 public class Home extends VerticalLayout {
+    private final Image main = new Image("image/mainPage.png", "Money");
     public Home() {
+    
         H1 header = new H1("Welcome to MoneyMaster, your Money Managing Machine!");
         add(header);
         setHorizontalComponentAlignment(Alignment.CENTER, header);
         
-        Image main = new Image("image/mainPage.png", "Money");
         main.setWidth("80em");
         add(main);
     }
